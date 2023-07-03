2530bcb7e920fa7ff3eb311439dde8c6c63d6fd5
Caio Damasceno <caio.damasceno@aluno.ufop.edu.br>
2023-07-03 13:10:52 -0300

    Prototipo de NavBar

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // Arquivo responsavel pela logica do site
+
+/* main.js */
+document.querySelector('.dropbtn').addEventListener('click', () => {
+    const dropdownContent = document.getElementById('dropdown');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.color = 'black'; /* Define explicitamente a cor do texto quando o menu dropdown é exibido */
+    }
+});
