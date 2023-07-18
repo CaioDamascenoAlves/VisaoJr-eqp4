a4ec84af67288e7de7bcc203efefcce5fe927f8f
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-07-18 10:56:37 -0300

    Store Vuex

---
+++ Vue/src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    currentVariant: "#1D2547",
+    navbarColor: "#1D2547",
+    textColor: "#D6A248",
+  },
+  mutations: {
+    updateVariant(state, variant) {
+      state.currentVariant = variant;
+    },
+    updateNavbarColor(state, navbarColor) {
+      state.navbarColor = navbarColor;
+    },
+    updateTextColor(state, textColor) {
+      state.textColor = textColor;
+    },
+  },
+  actions: {
+    setCurrentVariant({ commit }, variant) {
+      commit("updateVariant", variant);
+    },
+    setNavbarColor({ commit }, color) {
+      commit("updateNavbarColor", color);
+    },
+    setTextColor({ commit }, color) {
+      commit("updateTextColor", color);
+    },
+  },
+
+  modules: {},
+});
