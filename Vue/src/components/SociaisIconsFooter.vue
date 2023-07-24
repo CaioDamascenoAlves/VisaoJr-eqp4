9125d274bb27d7556d9430c00cc1953181b29b9d
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-07-23 22:54:10 -0300

    Componente para renderizar a redes sociais

---
+++ Vue/src/components/SociaisIconsFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="social-icons-footer">
+    <a href="#" class="social-icon-link">
+      <font-awesome-icon :icon="['fab', 'fa-instagram']" class="icon fa-2x" />
+    </a>
+    <a href="#" class="social-icon-link">
+      <font-awesome-icon :icon="['fab', 'fa-facebook']" class="icon fa-2x" />
+    </a>
+    <a href="#" class="social-icon-link">
+      <font-awesome-icon :icon="['fab', 'fa-whatsapp']" class="icon fa-2x" />
+    </a>
+    <a href="#" class="social-icon-link">
+      <font-awesome-icon :icon="['fab', 'fa-youtube']" class="icon fa-2x" />
+    </a>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "SocialIconsFooter",
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
+<style>
+.social-icons-footer {
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #1d2547;
+  padding: 5px;
+  z-index: 999;
+}
+
+.social-icon-link {
+  margin-left: 10px;
+}
+
+.icon {
+  color: #d6a248;
+}
+</style>
